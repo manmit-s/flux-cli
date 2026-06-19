99bc809588418202a574aced1f8b1ac1c5adb903
manmit <manmitsamal84@gmail.com>
2026-06-19 19:03:14 +0530

    Stream responses then process tools and update the context for LLM responses

--- agent/agent.py
+++ agent/agent.py
@@ -1,8 +1,9 @@
 from __future__ import annotations
+from pathlib import Path
 from typing import AsyncGenerator
 from agent.events import AgentEvent, AgentEventType
 from client.llm_client import LLMClient
-from client.response import StreamEventType
+from client.response import StreamEventType, ToolCall, ToolResultMessage
 from context.manager import ContextManager
 from tools.registry import create_default_registry
 
@@ -42,22 +43,66 @@ class Agent:
 
         tool_schemas = self.tool_registry.get_schemas()
 
+        tool_calls: list[ToolCall] = []
+
         async for event in self.client.chat_completion(
             self.context_manager.get_messages(), 
             tools=tool_schemas if tool_schemas else None):
+            
             if(event.type == StreamEventType.TEXT_DELTA and event.text_delta):
                 content = event.text_delta.content
                 response_text += content
                 yield AgentEvent.text_delta(content)
+
+            elif event.type == StreamEventType.TOOL_CALL_COMPLETE:
+                if event.tool_call:
+                    tool_calls.append(event.tool_call)
+
             
             elif event.type == StreamEventType.ERROR:
                 yield AgentEvent.agent_error(
                     event.error or "Unknown error occurred"
                 )
 
-        self.context_manager.add_user_message(response_text or None, )
+        self.context_manager.add_user_message(response_text or None,)
+
         if response_text:
             yield AgentEvent.text_complete(response_text)
+        
+        tool_call_results: list[ToolResultMessage] = []
+
+        for tool_call in tool_calls:
+            yield AgentEvent.tool_call_start(
+                tool_call.call_id,
+                tool_call.name,
+                tool_call.arguments,
+            )
+
+            result = await self.tool_registry.invoke(
+                tool_call.name,
+                tool_call.arguments,
+                Path.cwd(),
+            )
+
+            yield AgentEvent.tool_call_complete(
+                tool_call.call_id,
+                tool_call.name,
+                result,
+            )
+
+            tool_call_results.append(
+                ToolResultMessage(
+                    tool_call_id=tool_call.call_id,
+                    content=result.to_model_output(),
+                    is_error= not result.success,
+                )
+            )
+
+        for tool_result in tool_call_results:
+            self.context_manager.add_tool_result(
+                tool_result.tool_call_id,
+                tool_result.content, 
+            )  
     
     async def __aenter__(self) -> Agent:
         return self
