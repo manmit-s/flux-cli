00d393c207c1b253f5461a70630c0d605aae0cc4
manmit <manmitsamal84@gmail.com>
2026-06-24 20:34:58 +0530

    feat: register WriteFileTool in builtin tools registry

--- tools/builtin/__init__.py
+++ tools/builtin/__init__.py
@@ -1,9 +1,10 @@
 from tools.builtin.read_file import ReadFileTool
+from tools.builtin.write_file import WriteFileTool
 
 __all__ = ["ReadFileTool"]
 
 def get_all_builtin_tools() -> list[type]:
     return[
         ReadFileTool,
-        
+        WriteFileTool,
     ]
