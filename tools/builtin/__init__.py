c18471c1a5de679bfc1523ce941a20da766ce9f8
manmit <manmitsamal84@gmail.com>
2026-06-25 22:01:18 +0530

    feat: register EditTool, ShellTool, ListDirTool in builtin tools

--- tools/builtin/__init__.py
+++ tools/builtin/__init__.py
@@ -1,10 +1,24 @@
+from tools.builtin.edit_file import EditTool
+from tools.builtin.list_dir import ListDirTool
 from tools.builtin.read_file import ReadFileTool
+from tools.builtin.shell import ShellTool
 from tools.builtin.write_file import WriteFileTool
 
-__all__ = ["ReadFileTool"]
+__all__ = [
+    "ReadFileTool",
+    "WriteFileTool",
+    "EditTool",
+    "ShellTool",
+    "ListDirTool",
+
+    ]
 
 def get_all_builtin_tools() -> list[type]:
     return[
         ReadFileTool,
         WriteFileTool,
+        EditTool,
+        ShellTool,
+        ListDirTool,
+
     ]
